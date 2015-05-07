c419e1147b7ee551fc3821ecb2a5235da4a95ff2
Tobias Bohman <tobias.bohman@elev.it-gymnasiet.se>
2015-05-07 11:19:26 +0200

    added requirements for signing in & added db test user + link

--- app/db/seed.rb
+++ app/db/seed.rb
@@ -1,7 +1,21 @@
 class Seeder
 
   def self.seed!
+    self.user
+    self.link
+  end
 
+  def self.user
+    User.create(username: 'user',
+                password: 'test_password')
+  end
+
+  def self.link
+    Link.create(title: 'Google',
+                link: 'http://www.google.com',
+                tag: 'Search Engine',
+                secret: false,
+                user_id: 1)
   end
 
 end
