dfac8593fcc2e6c1d991e5c44d64db446432c2c3
Tobias Bohman <tobias.bohman@elev.it-gymnasiet.se>
2015-05-18 11:36:50 +0200

    added email support

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User
 
   property :id, Serial
   property :username, String, required: true, unique: true
+  property :email, String, required: false, unique: false
   property :password, BCryptHash, required: true
 
   has n, :links
@@ -19,8 +20,11 @@ class User
   end
 
   def self.register(params, app)
+    @email = ''
+    @email = params['email'] if params['email'].length > 0
     if params['password'] == params['confirm-password']
       User.create(username: params['username'],
+                  email: params['email'],
                   password: params['password'])
     else
       app.redirect '/404'
